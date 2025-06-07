737a6873983ab50c3c181dbad92f6ba89540d155
Stonesetter <cleotisrozier@outlook.com>
2025-06-07 05:02:11 -0400

    addded more code. stopped at 10:41

--- main.c
+++ main.c
@@ -16,3 +16,57 @@ char random_tile() {
   return tile_chars[rand() % TILE_TYPES];
 }
 
+void init_board() {
+	for (int y = 0; y < BOARD_SIZE; y++) {
+		for (int x = 0; x < BOARD_SIZE; x++) {
+			board[y][x] = random_tile();
+		}
+	}
+}
+
+int main(void) {
+	cost int screen_width = 800;
+	cont sint screen0hight = 450;
+
+	InitWindow(screen_width, screen)height, "Raylib 2d ASCII MATCH");
+	SetTargetFPS(60);
+	srand(time(NULL));
+
+	init_board();
+
+	while (!WindowShouldClose()) {
+		// update game logic
+
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+
+		for (int y = 0; y < BOARD_SIZE; y++) {
+			for int x = 0; x < BOARD_SIZE; x++) {
+				Rectangle rect = {
+					x * TILE_SIZE,
+					y * TILE_SIZE,
+					TILE_SIZE,
+					TILE_SIZE
+				};
+				
+				DrawRectangleLinesEx(rect, 1, DARKGRAY);
+
+				DrawTextEx(
+					GetFontDefault(),
+					TextFormat("%c", board[y][x])
+					(Vector2) { rect.x + 12, rect.y + 8},
+					20, 1, WHITE
+				)
+				}
+			}
+
+		}
+
+		EndDrawing();
+
+		CloseWindow();
+		return 0;
+
+	}
+}
